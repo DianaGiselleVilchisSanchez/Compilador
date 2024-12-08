3b61a753fb2936533b2c5333303b3f0f2ef7fbd0
Diana Giselle Vilchis Sánchez <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-12-07 23:13:42 -0600

    Add files via upload

---
+++ Tokens.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package codigo;
+
+/**
+ *
+ * @author yosyo
+ */
+public enum Tokens {
+    Reservadas,
+    Import,
+    Package,
+    Punto,
+    FinalLinea,
+    ParentesisIzquierdo,
+    ParentesisDerecho,
+    LlaveIzquierda,
+    LlaveDerecha,
+    CorcheteIzquierdo,
+    CorcheteDerecho,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Modulo,
+    Incremento,
+    Decremento,
+    MenorQue,
+    MayorQue,
+    MenorIgual,
+    MayorIgual,
+    Asignacion,
+    Diferente,
+    And,
+    Or,
+    Not,
+    ComillasDobles,
+    ComentarioLinea,
+    ComentarioParrafoAbre,
+    ComentarioParrafoCierra,
+    PalabrasReservadas,
+    Identificador,
+    Numero,
+    Igual,
+    ERROR,
+}
